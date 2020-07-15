d48ad45c3ffa63f6a301131a395596dfcaf7927b
Ayaz <iamayazhussain@gmail.com>
2020-07-15 20:02:06 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import React, { Component } from "react";
-
 import BootstrapTable from "react-bootstrap-table-next";
-
 import axios from "axios";
+import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
+import paginationFactory from "react-bootstrap-table2-paginator";
 
-export class Bootstraptab extends Component {
+class Bootstraptab extends Component {
   state = {
     employee: [],
 
     columns: [
       {
         dataField: "id",
-
         text: "ID",
       },
 
       {
         dataField: "userId",
-
         text: "UId",
       },
       {
         dataField: "title",
-
         text: "Title",
-
-        sort: true,
+        filter: textFilter(),
       },
       {
         dataField: "completed",
-
         text: "Com",
 
         sort: true,
@@ -44,22 +39,56 @@ export class Bootstraptab extends Component {
     });
   }
   render() {
+    const options = {
+      page: 2,
+
+      sizePerPageList: [
+        {
+          text: "5",
+          value: 5,
+        },
+        {
+          text: "10",
+          value: 10,
+        },
+        {
+          text: "All",
+          value: this.state.employee.length,
+        },
+      ],
+
+      sizePerPage: 5,
+
+      pageStartIndex: 0,
+
+      paginationSize: 3,
+
+      prePage: "Prev",
+
+      nextPage: "Next",
+
+      firstPage: "First",
+
+      lastPage: "Last",
+    };
     return (
       <div className="container">
         <div class="row" className="hdr">
           <div class="col-sm-12 btn btn-info">
-            React Bootstrap Data Table with Api
+            React Bootstrap Table with Searching and Custom Pagination
           </div>
         </div>
 
-        <div style={{ marginTop: 20 }}>
+        <div className="container" style={{ marginTop: 50 }}>
           <BootstrapTable
             striped
             hover
             keyField="id"
             data={this.state.employee}
             columns={this.state.columns}
-          ></BootstrapTable>
+            filter={filterFactory()}
+            pagination={paginationFactory(options)}
+          />
         </div>
       </div>
     );
