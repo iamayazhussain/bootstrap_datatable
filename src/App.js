cbd9747c682e86a5656da04c7a551accf70a7ddb
Ayaz <iamayazhussain@gmail.com>
2020-07-15 19:36:58 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BootstrapTable from "react-bootstrap-table-next";
+
+import axios from "axios";
+
+export class Bootstraptab extends Component {
+  state = {
+    employee: [],
+
+    columns: [
+      {
+        dataField: "id",
+
+        text: "ID",
+      },
+
+      {
+        dataField: "userId",
+
+        text: "UId",
+      },
+      {
+        dataField: "title",
+
+        text: "Title",
+
+        sort: true,
+      },
+      {
+        dataField: "completed",
+
+        text: "Com",
+
+        sort: true,
+      },
+    ],
+  };
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/todos/`).then(res => {
+      const employee = res.data;
+      this.setState({ employee });
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <div class="row" className="hdr">
+          <div class="col-sm-12 btn btn-info">
+            React Bootstrap Data Table with Api
+          </div>
+        </div>
+
+        <div style={{ marginTop: 20 }}>
+          <BootstrapTable
+            striped
+            hover
+            keyField="id"
+            data={this.state.employee}
+            columns={this.state.columns}
+          ></BootstrapTable>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Bootstraptab;
